aeba82c7bac8995e0a4e0d6a522a8ab7963d478d
sandragg <sandragg@yandex.ru>
2018-11-14 14:19:38 +0300

    Create router and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import * as React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { ROUTES } from './services/routes';
+import { Header } from './components/header/header';
 
 const App = () => {
 	return (
-		<div className="App" />
+		<BrowserRouter>
+			<div className="App">
+				<Header />
+				<main>
+					<Route exact path={ROUTES.HOME} />
+					<Route path={ROUTES.ALGS_AND_DS} />
+					<Route path={ROUTES.ABOUT} />
+				</main>
+			</div>
+		</BrowserRouter>
 	);
 };
 
---
+++ src/services/routes.tsx
@@ -0,0 +1,5 @@
+export const ROUTES = {
+	'HOME': '/',
+	'ALGS_AND_DS': '/gallery',
+	'ABOUT': '/about'
+};
