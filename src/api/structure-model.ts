8bfc42308b848fe10f7b2b6238a084aa0e08d05f
sandragg <sandragg@yandex.ru>
2019-01-24 17:49:16 +0300

    Fixed api path

--- src/api/structure-model.ts
+++ src/api/structure-model.ts
@@ -2,8 +2,8 @@ import 'whatwg-fetch';
 import { Structure } from 'src/services/interface';
 
 export class StructureModel {
-	public static EXTENDED_API_PATH: string = 'extended-structures.json';
-	public static API_PATH: string = 'structures.json';
+	public static EXTENDED_API_PATH: string = '/extended-structures.json';
+	public static API_PATH: string = '/structures.json';
 
 	public static getList(isExtended: boolean): Promise<Structure[]> {
 		return fetch(isExtended ? StructureModel.EXTENDED_API_PATH : StructureModel.API_PATH)
@@ -15,6 +15,6 @@ export class StructureModel {
 		return fetch(isExtended ? StructureModel.EXTENDED_API_PATH : StructureModel.API_PATH)
 			.then(res => res.json())
 			.then(structureList => structureList.find(struct => struct.id === id))
-			.catch(err => [err]);
+			.catch(err => err);
 	}
 }
