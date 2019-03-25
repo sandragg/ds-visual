d67dbebd30af115635024f3bb60cd886e4dd37b8
sandragg <sandragg@yandex.ru>
2019-03-25 22:44:31 +0300

    Updated Node component style

--- src/components/node/node.tsx
+++ src/components/node/node.tsx
@@ -1,52 +1,36 @@
 import React, {
 	useMemo,
 	RefObject,
-	HTMLAttributes,
-	SVGAttributes
+	ReactNode
 } from 'react';
+import {
+	calcTransformValue,
+	calcNodeMatrix
+} from 'src/services/helpers';
+import { PlainObject } from 'react-move/core';
 import './node.css';
-import { calcTransformValue, calcNodeMatrix } from 'src/services/helpers';
-import { NODE_OPTIONS } from 'src/services/constants';
-import { Point } from 'src/services/interface';
 
-interface Props extends HTMLAttributes<HTMLElement> {
+interface Props {
 	nodeRef?: RefObject<SVGGElement>,
-	attrs?: SVGAttributes<SVGElement>
+	attrs?: PlainObject,
+	children: ReactNode[]
 }
-
-const nodeCenterPoint: Point = {
-	x: NODE_OPTIONS.WIDTH / 2,
-	y: NODE_OPTIONS.HEIGHT / 2
-};
 
 export const Node = ({ children, nodeRef, attrs }: Props) => {
 	const matrix: number[] = useMemo(
 	() => calcNodeMatrix({ x: Number(attrs.x), y: Number(attrs.y) }),
 	[attrs.x, attrs.y]
 	);
-	const transformValue: string = calcTransformValue(attrs);
+	const transform: string = calcTransformValue(attrs);
 
 	return (
 		<g
 			ref={nodeRef}
 			className="node"
-			transform={`matrix(${matrix}) ${transformValue}`}
+			transform={`matrix(${matrix}) ${transform}`}
 			{...attrs}
 		>
-			<rect
-				className="node__figure"
-				height={NODE_OPTIONS.HEIGHT}
-				width={NODE_OPTIONS.WIDTH}
-				rx="6"
-				fill={attrs.fill}
-			/>
-			<text
-				className="node__caption"
-				x={nodeCenterPoint.x}
-				y={nodeCenterPoint.y}
-			>
-				{children}
-			</text>
+			{children}
 		</g>
 	);
 };
